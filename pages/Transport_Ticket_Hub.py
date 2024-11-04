c57ed3fd1eafc617fdd0d629ce7fd5b27b5df6f0
Emma McLuckie <emmamcluckie@outlook.com>
2024-11-05 10:28:15 +1100

    fixed the images thing sorry

--- pages/Transport_Ticket_Hub.py
+++ pages/Transport_Ticket_Hub.py
@@ -73,14 +73,14 @@ prompt_id_use = "322d5421-8fba-4ac2-ae39-8b4e995c05b9"  # Second generation for 
 prompt_id_terms = "9d417150-c210-4151-9850-ad97a3bd0c56"  # Third generation for ticket use
 
 if submit_button:
-    images, ticket_names, ticket_machine_image = [], [], None
+    ticket_images, ticket_names, ticket_machine_image = [], [], None
 
-    # Retrieve images and ticket names based on config
+    # Retrieve ticket_images and ticket names based on config
     travel_key = travel_type.lower()
     ticket_key = ticket_type.lower()
 
     if state in config and travel_key in config[state] and ticket_key in config[state][travel_key]:
-        images = config[state][travel_key][ticket_key]["images"]
+        ticket_images = config[state][travel_key][ticket_key]["ticket_images"]
         ticket_names = config[state][travel_key][ticket_key]["ticket_names"]
         ticket_machine_image = config[state]["ticket_machine_image"]
     else:
@@ -100,9 +100,9 @@ if submit_button:
     recommendation_output = wordware(inputs, prompt_id_recommendation, api_key)
     if recommendation_output:
         st.write(recommendation_output)
-        # Display images and captions only after the first generation output
-        if images:
-            st.image(images, caption=ticket_names, width=200)
+        # Display ticket_images and captions only after the first generation output
+        if ticket_images:
+            st.image(ticket_images, caption=ticket_names, width=200)
 
     # Second generation: Ticket use
     use_output = wordware(inputs, prompt_id_use, api_key)
